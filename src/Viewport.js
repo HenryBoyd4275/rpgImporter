92ec1cacd513384491e48163e30bcaf9c08b7b92
Henry Boyd <amorphousCube@gmail.com>
2020-05-21 16:29:32 -0400

    added basic timekeeping

--- src/Viewport.js
+++ src/Viewport.js
@@ -1,29 +1,69 @@
 import React, { useState } from "react";
-import { Box } from "grommet";
+import { Box, Header } from "grommet";
 import Contextbar from "./Contextbar";
 import PartyBar from "./PartyBar";
-//import execute from './optionDriver.js';
 
 export default function Viewport() {
   const [node, setNode] = useState(require(`./Nodes/1A.json`));
+  const [time, setTime] = useState([0,0,0]);
+
+  function addTime(input) {
+    let value = input.slice(0, -1);
+    console.log(value, input[input.length - 1])
+    let temp = time;
+    switch (input[input.length - 1]) {
+      case "d":
+        temp[0] += 1*value;
+        break;
+      case "h":
+        temp[1] += 1*value;
+        break;
+      case "m":
+        temp[2] += 1*value;
+        break;
+    }
+    if (temp[2] >= 60) {
+      temp[1] += 1;
+      temp[2] -= 60;
+    }
+    if (temp[1] >= 60) {
+      temp[0] += 1;
+      temp[1] -= 60;
+    }
+    setTime(temp);
+  }
+
+  function displayTime() {
+    return time.map((element, index) => {
+      if (index === 0) {
+        if (element < 10) {
+          return `0${element}`
+        }
+      }
+      else {
+        if (element < 10) {
+          return `:0${element}`
+        }
+      }
+    })
+  }
 
   function goTo(key) {
     setNode(require(`./Nodes/${key}.json`));
   }
 
-  function execute(inputs) {
-    let effect = inputs.Effect.split(' ');
+  function execute(input) {
+    let effect = input.Effect.split(' ');
 
     switch (effect[0]) {
       case "GoTo":
+        addTime(input.Time);
         goTo(effect[1]);
         break;
       default:
         console.log(`Command ${effect[0]} not found`)
     }
   }
-
-  console.log("node:", node);
 
   return (
     <Box
@@ -33,7 +73,7 @@ export default function Viewport() {
       direction="column"
     >
       <Box pad="medium" height="100%" background="neutral-2">
-        <Box align="center">{node.Name}</Box>
+        <Header align="center">Map {node.Name} <Box>{displayTime()}</Box></Header>
         <Box
           pad="small"
           border={{ color: "brand", size: "large" }}
